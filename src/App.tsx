b475b57ec4c86dd3a8570df2f1f3ec39cb79f828
johndevnoza <ioandevnoza@gmail.com>
2024-07-29 18:51:53 +0400

    Added Vercel analitycs, Removed Consolelogs

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,22 @@ import Home from "pages/Home";
 import Skills from "pages/Skills";
 import SocialMedia from "pages/SocialMedia";
 import { Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
-  
   return (
-    <Routes>
-      <Route path="/" element={<Home />}>
-        <Route path="/about" element={<About />} />
-        <Route path="/skills" element={<Skills />} />
-        <Route path="/social-media" element={<SocialMedia />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/game" element={<Game />} />
-      </Route>
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route path="/about" element={<About />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/social-media" element={<SocialMedia />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/game" element={<Game />} />
+        </Route>
+      </Routes>
+      <Analytics />
+    </>
   );
 }
 
